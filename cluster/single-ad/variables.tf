a6ae60074da6e82d89aa720178e08ff89a1ad7de
lfeldman <lukasz.feldman@oracle.com>
2020-11-27 11:05:55 +0100

    More flexible ES and Kibana version pickup v4

--- cluster/single-ad/variables.tf
+++ cluster/single-ad/variables.tf
@@ -1,13 +1,8 @@
 variable "tenancy_ocid" {}
-
 variable "user_ocid" {}
-
 variable "fingerprint" {}
-
 variable "private_key_path" {}
-
 variable "region" {}
-
 variable "compartment_ocid" {}
 
 variable "BastionShape" {
